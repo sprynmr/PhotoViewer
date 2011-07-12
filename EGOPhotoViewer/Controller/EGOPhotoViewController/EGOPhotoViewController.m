0d35f3fc5b9911803cf003303aa7d94c0cc32579
Bob Spryn <bobspryn@gmail.com>
2011-07-11 23:30:15 -0600

    small fix to stop some animation weirdness

--- EGOPhotoViewer/Controller/EGOPhotoViewController/EGOPhotoViewController.m
+++ EGOPhotoViewer/Controller/EGOPhotoViewController/EGOPhotoViewController.m
@@ -787,13 +787,9 @@
 			EGOPhotoImageView *_photoView = (EGOPhotoImageView*)[self.photoViews objectAtIndex:page];
 			CGRect newframe = CGRectMake(originX, 0.0f, self.scrollView.bounds.size.width, self.scrollView.bounds.size.height);
 			
-			if (!CGRectEqualToRect(_photoView.frame, newframe)) {	
-				
-				[UIView beginAnimations:nil context:NULL];
-				[UIView setAnimationDuration:0.1];
+			if (!CGRectEqualToRect(_photoView.frame, newframe)) {
+                // no idea why someone animated this except for maybe hardware acceleration, but it causes problems
 				_photoView.frame = newframe;
-				[UIView commitAnimations];
-			
 			}
 			
 		}
